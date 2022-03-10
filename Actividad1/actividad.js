805519dcb3a1c02ea4be7cc0bfe474cb615ce54a
CaroSnow1 <99303897+CaroSnow1@users.noreply.github.com>
2022-03-09 18:40:08 -0600

    Add files via upload

---
+++ Actividad1/actividad.js
@@ -0,0 +1,15 @@
+// P U N T O   1 
+var numero = Number(prompt("Ingresa un numero"))
+var multiplicacion = numero * 2
+console.log(multiplicacion)
+// P U N T O   2
+var numero1 = Number(prompt("Ingresa el número 1"))
+var numero2 = Number(prompt("Ingresa el número 2"))
+var resta = numero1 - numero2
+console.log(resta)
+// P U N T O   3
+var area = 3.1416 * (5.3 ** 2)
+console.log(area)
+// P U N T O 4 
+var areaRombo = 8*6/2
+console.log(areaRombo)
